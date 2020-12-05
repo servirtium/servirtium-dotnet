077c6c62a502a022a3de8a607820f6a071f71f3c
Stephen Hand <steve2727@hotmail.com>
2020-12-05 16:39:29 +0000

    Missing files

---
+++ Servirtium.Core/Http/FindAndReplaceHttpMessageTransforms.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Servirtium.Core.Interactions;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Servirtium.Core.Http
+{
+    public class FindAndReplaceHttpMessageTransforms : IHttpMessageTransforms
+    {
+        private readonly ILogger<FindAndReplaceHttpMessageTransforms> _logger;
+
+        private readonly IEnumerable<RegexReplacement> _replacementsForRecording;
+
+
+        public FindAndReplaceHttpMessageTransforms(IEnumerable<RegexReplacement> replacementsForRecording, ILoggerFactory? loggerFactory = null)
+        {
+            _logger = (loggerFactory ?? NullLoggerFactory.Instance).CreateLogger<FindAndReplaceHttpMessageTransforms>();
+            _replacementsForRecording = replacementsForRecording;
+        }
+
+        public IRequestMessage TransformClientRequestForRealService(IRequestMessage clientRequest)
+        {
+            var builder = new ServiceRequest.Builder()
+                .From(clientRequest)
+                .Headers(clientRequest.Headers.Select(h => RegexReplacement.FixHeaderForRecording(h, _replacementsForRecording, ReplacementContext.RequestHeader)));
+
+            if (clientRequest.Body.HasValue && TextAndBinaryBodyFormatter.IsTextMediaType(clientRequest.Body.Value.Type))
+            {
+                builder = builder.Body(
+                    RegexReplacement.FixStringForRecording(Encoding.UTF8.GetString(clientRequest.Body.Value.Content), _replacementsForRecording, ReplacementContext.RequestBody),
+                    clientRequest.Body.Value.Type
+                );
+            }
+            
+            return builder
+                .Build();
+        }
+
+        public IResponseMessage TransformRealServiceResponseForClient(IResponseMessage serviceResponse)
+        {
+            var builder = new ServiceResponse.Builder()
+                .From(serviceResponse)
+                .Headers(serviceResponse.Headers.Select(h => RegexReplacement.FixHeaderForRecording(h, _replacementsForRecording, ReplacementContext.ResponseHeader)));
+
+            if (serviceResponse.Body.HasValue && TextAndBinaryBodyFormatter.IsTextMediaType(serviceResponse.Body.Value.Type))
+            {
+                builder = builder.Body(
+                    RegexReplacement.FixStringForRecording(Encoding.UTF8.GetString(serviceResponse.Body.Value.Content), _replacementsForRecording, ReplacementContext.ResponseBody), 
+                    serviceResponse.Body.Value.Type
+                );
+            }
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Servirtium.Core/Http/HttpMessageTransformPipeline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Servirtium.Core.Http
+{
+    public class HttpMessageTransformPipeline : IHttpMessageTransforms
+    {
+        IEnumerable<IHttpMessageTransforms> _transforms;
+
+        public HttpMessageTransformPipeline(params IHttpMessageTransforms[] transforms)
+        {
+            _transforms = transforms;
+        }
+
+
+        public IRequestMessage TransformClientRequestForRealService(IRequestMessage clientRequest)
+        {
+            return  _transforms.Aggregate(clientRequest, (request, transform) => transform.TransformClientRequestForRealService(request));
+        }
+
+        public IResponseMessage TransformRealServiceResponseForClient(IResponseMessage serviceResponse)
+        {
+            return _transforms.Aggregate(serviceResponse, (response, transform) => transform.TransformRealServiceResponseForClient(response));
+        }
+    }
+}
